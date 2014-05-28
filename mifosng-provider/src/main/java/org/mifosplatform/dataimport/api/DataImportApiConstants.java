8331420d8aab7649817b0b6ac2d3c378aff12184
diunuge <diunuge.10@cse.mrt.ac.lk>
2014-05-29 01:18:21 +0530

    Added DataImportApiConstants.java

---
+++ mifosng-provider/src/main/java/org/mifosplatform/dataimport/api/DataImportApiConstants.java
@@ -0,0 +1,14 @@
+package org.mifosplatform.dataimport.api;
+
+public class DataImportApiConstants {
+
+	public static String mifosApiEndPoint = "https://localhost:8443/mifosng-provider/api/v1/";
+	public static String mifosUser = "mifos";
+	public static String mifosPassword = "password";
+	public static String tenantID = "default";
+
+    public DataImportApiResource() {
+        
+		
+    }
+}
