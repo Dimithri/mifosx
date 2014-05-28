e51fe4b7515a484339a1495914481cee26264a8a
diunuge <diunuge.10@cse.mrt.ac.lk>
2014-05-28 23:34:15 +0530

    Added initial API Class to package ../api

---
+++ mifosng-provider/src/main/java/org/mifosplatform/dataimport/api/DataImportApiResource.java
@@ -0,0 +1,13 @@
+package org.mifosplatform.dataimport.api;
+
+@Path("/dataimport")
+@Component
+@Scope("singleton")
+public class DataImportApiResource {
+
+    @Autowired
+    public DataImportApiResource() {
+        
+		
+    }
+}
