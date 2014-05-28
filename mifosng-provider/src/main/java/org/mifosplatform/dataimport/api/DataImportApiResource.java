617d713c4df6252cb421a3162406b35ccd90fdce
diunuge <diunuge.10@cse.mrt.ac.lk>
2014-05-29 01:17:14 +0530

    DataImport API Interface

--- mifosng-provider/src/main/java/org/mifosplatform/dataimport/api/DataImportApiResource.java
+++ mifosng-provider/src/main/java/org/mifosplatform/dataimport/api/DataImportApiResource.java
@@ -10,4 +10,32 @@ public class DataImportApiResource {
         
 		
     }
+	
+	@GET
+    @Path("template")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+	public String retrieveTemplate(@Context final UriInfo uriInfo, @QueryParam("template") final String fileName,
+            @QueryParam("clientType") final String clientType){
+			
+			
+	}
+	
+	@POST
+    @Path("import")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+	public String importTemplate(@Context final UriInfo uriInfo, @QueryParam("file") final String fileName){
+			
+			
+	}
+	
+	@GET
+	@Path("test")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+	public String importTemplate(@Context final UriInfo uriInfo){
+			
+			
+	}
 }
